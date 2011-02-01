d7dcfd3383fe903697b6aac817df17426de2f5a7
Bryan O'Sullivan <bos@serpentine.com>
2011-02-01 10:10:34 -0800

    Fix the handling of signedness, which caused a nasty bug

--- cbits/hashByteString.c
+++ cbits/hashByteString.c
@@ -1,5 +1,5 @@
 /* Bernstein's hash */
-int djb_hash(const char* str, int len, int hash) {
+int djb_hash(const unsigned char* str, int len, int hash) {
 
   while (len--) {
     hash = (hash * 33) ^ *str++;
