a3c25f69dd18a92b458c5aac2b09f52b0ff198ee
Bryan O'Sullivan <bos@serpentine.com>
2012-10-01 21:59:58 -0700

    Add some comments to getRandomBytes

--- Data/Hashable/RandomSource.hs
+++ Data/Hashable/RandomSource.hs
@@ -11,6 +11,7 @@ import Data.Word (Word8)
 import Foreign.C.Error (throwErrnoIfMinus1_)
 import Foreign.C.Types (CInt(..))
 import Foreign.Ptr (Ptr)
+import Foreign.Marshal.Alloc
 
 getRandomBytes :: Int -> IO ByteString
 getRandomBytes nbytes
@@ -20,5 +21,11 @@ getRandomBytes nbytes
     throwErrnoIfMinus1_ "getRandomBytes" .
     c_getRandomBytes (fromIntegral nbytes)
 
+getRandom :: Storable a => IO a
+getRandom = do
+  alloca $ \ptr ->
+    throwErrnoIfMinus1_ "getRandomBytes" $
+    c_getRandomBytes (fromIntegral size) ptr
+
 foreign import ccall unsafe "hashable_getRandomBytes" c_getRandomBytes
-    :: CInt -> Ptr Word8 -> IO CInt
+    :: CInt -> Ptr a -> IO CInt
