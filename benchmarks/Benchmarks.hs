0a3f5803b7c02ea989752068a4d95a167eda9379
Bryan O'Sullivan <bos@serpentine.com>
2012-10-02 17:07:59 -0700

    Benchmark SSE 4.1 too

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -55,6 +55,9 @@ main = do
         cSipHash (PS fp off len) =
             inlinePerformIO . withForeignPtr fp $ \ptr ->
             return $! c_siphash 2 4 k0 k1 (ptr `plusPtr` off) (fromIntegral len)
+        sse41SipHash (PS fp off len) =
+            inlinePerformIO . withForeignPtr fp $ \ptr ->
+            return $! sse41_siphash k0 k1 (ptr `plusPtr` off) (fromIntegral len)
 
     withForeignPtr fp5 $ \ p5 ->
         withForeignPtr fp8 $ \ p8 ->
@@ -111,6 +114,15 @@ main = do
           , bench "512" $ whnf cSipHash bs512
           , bench "2^20" $ whnf cSipHash bs1Mb
           ]
+        , bgroup "sse41SipHash"
+          [ bench "5" $ whnf sse41SipHash bs5
+          , bench "8" $ whnf sse41SipHash bs8
+          , bench "11" $ whnf sse41SipHash bs11
+          , bench "40" $ whnf sse41SipHash bs40
+          , bench "128" $ whnf sse41SipHash bs128
+          , bench "512" $ whnf sse41SipHash bs512
+          , bench "2^20" $ whnf sse41SipHash bs1Mb
+          ]
         , bgroup "pkgSipHash"
           [ bench "5" $ whnf hsSipHash bs5
           , bench "8" $ whnf hsSipHash bs8
@@ -132,3 +144,5 @@ new (I# n#) = unBA (runST $ ST $ \s1 ->
 
 foreign import ccall unsafe "siphash" c_siphash
     :: CInt -> CInt -> Word64 -> Word64 -> Ptr Word8 -> CSize -> Word64
+foreign import ccall unsafe "siphash_sse41" sse41_siphash
+    :: Word64 -> Word64 -> Ptr Word8 -> CSize -> Word64
