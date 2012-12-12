8be4b338319c6611986be477e2f4b67f6f012097
Bryan O'Sullivan <bos@serpentine.com>
2012-12-11 23:14:28 -0800

    Benchmark lazy Text and ByteString

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -15,9 +15,11 @@ import Data.Word
 import Foreign.C.Types (CInt(..), CSize(..))
 import Foreign.Ptr
 import Data.ByteString.Internal
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
-import qualified Data.ByteString as B
+import qualified Data.Text.Lazy as TL
 import qualified Crypto.MAC.SipHash as HS
+import qualified Data.ByteString.Char8 as B8
 
 -- Benchmark English words (5 and 8), base64 encoded integers (11),
 -- SHA1 hashes as hex (40), and large blobs (1 Mb).
@@ -34,37 +36,34 @@ main = do
     fp1Mb <- mallocForeignPtrBytes mb
 
     -- We don't care about the contents of these either.
-    let !ba5 = new 5
-        !ba8 = new 8
-        !ba11 = new 11
-        !ba40 = new 40
-        !ba128 = new 128
-        !ba512 = new 512
-        !ba1Mb = new mb
+    let !ba5 = new 5;     !ba8 = new 8;     !ba11 = new 11; !ba40 = new 40
+        !ba128 = new 128; !ba512 = new 512; !ba1Mb = new mb
 
-    let !bs5 = B.pack [0..4]
-        !bs8 = B.pack [0..7]
-        !bs11 = B.pack [0..10]
-        !bs40 = B.pack [0..39]
-        !bs128 = B.pack [0..127]
-        !bs512 = B.pack . map fromIntegral $ [0..511::Int]
-        !bs1Mb = B.pack . map fromIntegral $ [0..999999::Int]
-
-        s5 = ['\0'..'\4']
-        s8 = ['\0'..'\7']
-        s11 = ['\0'..'\10']
-        s40 = ['\0'..'\39']
-        s128 = ['\0'..'\127']
-        s512 = ['\0'..'\511']
+        s5 = ['\0'..'\4'];   s8 = ['\0'..'\7'];     s11 = ['\0'..'\10']
+        s40 = ['\0'..'\39']; s128 = ['\0'..'\127']; s512 = ['\0'..'\511']
         s1Mb = ['\0'..'\999999']
 
-        !t5 = T.pack s5
-        !t8 = T.pack s8
-        !t11 = T.pack s11
-        !t40 = T.pack s40
-        !t128 = T.pack s128
-        !t512 = T.pack s512
+        !bs5 = B8.pack s5;   !bs8 = B8.pack s8;     !bs11 = B8.pack s11
+        !bs40 = B8.pack s40; !bs128 = B8.pack s128; !bs512 = B8.pack s512
+        !bs1Mb = B8.pack s1Mb
+
+        blmeg = BL.take (fromIntegral mb) . BL.fromChunks . repeat
+        bl5 = BL.fromChunks [bs5];     bl8 = BL.fromChunks [bs8]
+        bl11 = BL.fromChunks [bs11];   bl40 = BL.fromChunks [bs40]
+        bl128 = BL.fromChunks [bs128]; bl512 = BL.fromChunks [bs512]
+        bl1Mb_40 = blmeg bs40;         bl1Mb_128 = blmeg bs128
+        bl1Mb_64k = blmeg (B8.take 65536 bs1Mb)
+
+        !t5 = T.pack s5;   !t8 = T.pack s8;     !t11 = T.pack s11
+        !t40 = T.pack s40; !t128 = T.pack s128; !t512 = T.pack s512
         !t1Mb = T.pack s1Mb
+
+        tlmeg = TL.take (fromIntegral mb) . TL.fromChunks . repeat
+        tl5 = TL.fromStrict t5;     tl8 = TL.fromStrict t8
+        tl11 = TL.fromStrict t11;   tl40 = TL.fromStrict t40
+        tl128 = TL.fromStrict t128; tl512 = TL.fromChunks (replicate 4 t128)
+        tl1Mb_40 = tlmeg t40;       tl1Mb_128 = tlmeg t128
+        tl1Mb_64k = tlmeg (T.take 65536 t1Mb)
 
     let k0 = 0x4a7330fae70f52e8
         k1 = 0x919ea5953a9a1ec9
@@ -110,13 +109,26 @@ main = do
           ]
         , bgroup "hash"
           [ bgroup "ByteString"
-            [ bench "5" $ whnf hash bs5
-            , bench "8" $ whnf hash bs8
-            , bench "11" $ whnf hash bs11
-            , bench "40" $ whnf hash bs40
-            , bench "128" $ whnf hash bs128
-            , bench "512" $ whnf hash bs512
-            , bench "2^20" $ whnf hash bs1Mb
+            [ bgroup "strict"
+              [ bench "5" $ whnf hash bs5
+              , bench "8" $ whnf hash bs8
+              , bench "11" $ whnf hash bs11
+              , bench "40" $ whnf hash bs40
+              , bench "128" $ whnf hash bs128
+              , bench "512" $ whnf hash bs512
+              , bench "2^20" $ whnf hash bs1Mb
+              ]
+            , bgroup "lazy"
+                [ bench "5" $ whnf hash bl5
+                , bench "8" $ whnf hash bl8
+                , bench "11" $ whnf hash bl11
+                , bench "40" $ whnf hash bl40
+                , bench "128" $ whnf hash bl128
+                , bench "512" $ whnf hash bl512
+                , bench "2^20_40" $ whnf hash bl1Mb_40
+                , bench "2^20_128" $ whnf hash bl1Mb_128
+                , bench "2^20_64k" $ whnf hash bl1Mb_64k
+                ]
             ]
           , bgroup "String"
             [ bench "5" $ whnf hash s5
@@ -128,13 +140,26 @@ main = do
             , bench "2^20" $ whnf hash s1Mb
             ]
           , bgroup "Text"
-            [ bench "5" $ whnf hash t5
-            , bench "8" $ whnf hash t8
-            , bench "11" $ whnf hash t11
-            , bench "40" $ whnf hash t40
-            , bench "128" $ whnf hash t128
-            , bench "512" $ whnf hash t512
-            , bench "2^20" $ whnf hash t1Mb
+            [ bgroup "strict"
+              [ bench "5" $ whnf hash t5
+              , bench "8" $ whnf hash t8
+              , bench "11" $ whnf hash t11
+              , bench "40" $ whnf hash t40
+              , bench "128" $ whnf hash t128
+              , bench "512" $ whnf hash t512
+              , bench "2^20" $ whnf hash t1Mb
+              ]
+            , bgroup "lazy"
+              [ bench "5" $ whnf hash tl5
+              , bench "8" $ whnf hash tl8
+              , bench "11" $ whnf hash tl11
+              , bench "40" $ whnf hash tl40
+              , bench "128" $ whnf hash tl128
+              , bench "512" $ whnf hash tl512
+              , bench "2^20_40" $ whnf hash tl1Mb_40
+              , bench "2^20_128" $ whnf hash tl1Mb_128
+              , bench "2^20_64k" $ whnf hash tl1Mb_64k
+              ]
             ]
           , bench "Int8" $ whnf hash (0xef :: Int8)
           , bench "Int16" $ whnf hash (0x7eef :: Int16)
