9f8a92329efad8a14294e0006f7a279087e994bd
Bryan O'Sullivan <bos@serpentine.com>
2012-12-14 08:36:44 -0800

    Use 16-byte stack alignment on Windows, if using SSE

--- cbits/siphash.h
+++ cbits/siphash.h
@@ -14,9 +14,19 @@ typedef uint8_t u8;
 u64 hashable_siphash(int, int, u64, u64, const u8 *, size_t);
 u64 hashable_siphash24(u64, u64, const u8 *, size_t);
 
-#if defined(__i386)
-u64 hashable_siphash24_sse2(u64, u64, const u8 *, size_t);
-u64 hashable_siphash24_sse41(u64, u64, const u8 *, size_t);
+#if defined(__i386) || defined(__x86_64)
+
+/* To use SSE instructions on Windows, we have to adjust the stack
+   from its default of 4-byte alignment to use 16-byte alignment. */
+
+# if defined(_WIN32)
+#  define ALIGNED_STACK __attribute__((force_align_arg_pointer))
+# else
+#  define ALIGNED_STACK
+# endif
+
+u64 hashable_siphash24_sse2(u64, u64, const u8 *, size_t) ALIGNED_STACK;
+u64 hashable_siphash24_sse41(u64, u64, const u8 *, size_t) ALIGNED_STACK;
 #endif
 
 #endif /* _hashable_siphash_h */
