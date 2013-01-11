9691f619acd7881255931b581da66a496d9346b2
Bryan O'Sullivan <bos@serpentine.com>
2013-01-10 21:31:32 -0800

    SSE: force the stack to 16-byte alignment everywhere.
    This fixes gh-52.
    
    --HG--
    extra : rebase_source : b503958047fd9a91d87068f611ea9fb93c60178f

--- cbits/siphash.h
+++ cbits/siphash.h
@@ -16,14 +16,10 @@ u64 hashable_siphash24(u64, u64, const u8 *, size_t);
 
 #if defined(__i386) || defined(__x86_64)
 
-/* To use SSE instructions on Windows, we have to adjust the stack
-   from its default of 4-byte alignment to use 16-byte alignment. */
+/* To use SSE instructions, we have to adjust the stack from its
+   default of 4-byte alignment to use 16-byte alignment. */
 
-# if defined(_WIN32)
-#  define ALIGNED_STACK __attribute__((force_align_arg_pointer))
-# else
-#  define ALIGNED_STACK
-# endif
+# define ALIGNED_STACK __attribute__((force_align_arg_pointer))
 
 u64 hashable_siphash24_sse2(u64, u64, const u8 *, size_t) ALIGNED_STACK;
 u64 hashable_siphash24_sse41(u64, u64, const u8 *, size_t) ALIGNED_STACK;
