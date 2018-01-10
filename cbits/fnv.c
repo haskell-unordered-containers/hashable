ea9a32bfa2ab3b82f04dc66838d7d82f085ddb37
Karl Millar <kmillar@google.com>
2018-01-10 11:46:54 -0800

    Fix undefined behavior (signed integer overflow)

--- cbits/fnv.c
+++ cbits/fnv.c
@@ -36,8 +36,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * The FNV-1 hash description: http://isthe.com/chongo/tech/comp/fnv/
  * The FNV-1 hash is public domain: http://isthe.com/chongo/tech/comp/fnv/#public_domain
  */
-long hashable_fnv_hash(const unsigned char* str, long len, long hash) {
+long hashable_fnv_hash(const unsigned char* str, long len, long salt) {
 
+  unsigned long hash = salt;
   while (len--) {
     hash = (hash * 16777619) ^ *str++;
   }
@@ -48,6 +49,6 @@ long hashable_fnv_hash(const unsigned char* str, long len, long hash) {
 /* Used for ByteArray#s. We can't treat them like pointers in
    native Haskell, but we can in unsafe FFI calls.
  */
-long hashable_fnv_hash_offset(const unsigned char* str, long offset, long len, long hash) {
-  return hashable_fnv_hash(str + offset, len, hash);
+long hashable_fnv_hash_offset(const unsigned char* str, long offset, long len, long salt) {
+  return hashable_fnv_hash(str + offset, len, salt);
 }
