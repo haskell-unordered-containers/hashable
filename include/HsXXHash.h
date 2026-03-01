d43fcb884f2f8856070e91d39150ca00385e1675
OPNA2608 <opna2608@protonmail.com>
2026-03-01 14:14:26 +0100

    include/HsXXHash.h: Normalise byte order in withSeed_u{32,64}

--- include/HsXXHash.h
+++ include/HsXXHash.h
@@ -13,11 +13,28 @@ static inline uint64_t hs_XXH3_64bits_withSeed_offset(const uint8_t *ptr, size_t
 }
 
 static inline uint64_t hs_XXH3_64bits_withSeed_u64(uint64_t val, uint64_t seed) {
-    return XXH3_64bits_withSeed(&val, sizeof(val), seed);
+    unsigned int i;
+    uint8_t valAsArray[sizeof (val)];
+
+    for (i = 0; i < sizeof (val); ++i) {
+        valAsArray[i] = val & 0xFF;
+        val >>= 8;
+    }
+
+    return XXH3_64bits_withSeed(valAsArray, sizeof(val), seed);
 }
 
 static inline uint64_t hs_XXH3_64bits_withSeed_u32(uint32_t val, uint64_t seed) {
-    return XXH3_64bits_withSeed(&val, sizeof(val), seed);
+    unsigned int i;
+    uint8_t valAsArray[sizeof (val)];
+
+    for (i = 0; i < sizeof (val); ++i) {
+        valAsArray[i] = val & 0xFF;
+        val >>= 8;
+    }
+
+    return XXH3_64bits_withSeed(valAsArray, sizeof(val), seed);
+
 }
 
 static inline void hs_XXH3_64bits_update_offset(XXH3_state_t *statePtr, const uint8_t *ptr, size_t off, size_t len) {
